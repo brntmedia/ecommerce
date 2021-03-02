c75e8baceca76385687786c9565a25e9ca8d569f
Tyler <brntmedia@gmail.com>
2021-03-02 13:10:29 -0500

    lunch

--- ecommerce/src/components/Products/Products.jsx
+++ ecommerce/src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles'
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes', price:'$5', image:'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-8a39d2ed-c6f8-4183-93a9-69cf4dbd0cb8/react-miler-womens-running-shoe-0mgX0d.jpg'},
@@ -9,15 +10,18 @@ const products = [
 ]
 
 const Products = ()=> {
+    
+    const classes = useStyles();
     return(
-        <main>
-        <Grid container justify="center" spacing={4}>
-            {products.map((product)=> (
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
-                </Grid>
-            ))}
-        </Grid>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product)=> (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product}/>
+                    </Grid>
+                ))}
+            </Grid>
     </main>
     )
     
