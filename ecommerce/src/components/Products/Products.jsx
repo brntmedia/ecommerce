7a897d54faef51cf1574e9e993a96cb8be1d979f
Tyler <brntmedia@gmail.com>
2021-03-02 17:15:44 -0500

    break

--- ecommerce/src/components/Products/Products.jsx
+++ ecommerce/src/components/Products/Products.jsx
@@ -4,12 +4,9 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes', price:'$5', image:'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-8a39d2ed-c6f8-4183-93a9-69cf4dbd0cb8/react-miler-womens-running-shoe-0mgX0d.jpg'},
-    { id: 2, name: 'Macbook', description: 'Apple Macbook', price:'$10', image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1603332211000'}
-]
 
-const Products = ()=> {
+
+const Products = ({products})=> {
     
     const classes = useStyles();
     return(
---
+++ ecommerce/src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce('pk_240039ec1696281efbd3998cb989b6d4358ba9ce31f31')
